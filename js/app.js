86786bd0fd00978c1373b100013f2e84b1a9bc36
Lucas Ramos <lucasrcdias@gmail.com>
2015-03-27 23:44:53 -0300

    Atualização da semana

--- js/app.js
+++ js/app.js
@@ -1,108 +1,133 @@
-$(function(){
+$(function () {
   //clique no botão de pesquisa por voz
-  $(".search-toggle").click(function(){
-    $(".voice-overlay").fadeIn(function(){
-      if(!mob)
+  $(".search-toggle").click(function () {
+    $(".voice-overlay").fadeIn(function () {
+      if (!mob)
         $(".voice-search").animate({
-          "bottom":"50%",
-          "margin-bottom":"-36px"
-        }, function(){
+          "bottom": "50%",
+          "margin-bottom": "-36px"
+        }, function () {
           $(this).addClass("clicked");
         });
       else
         $(".voice-search").animate({
-          "bottom":"30%",
-          "margin-bottom":"-36px"
-        }, function(){
+          "bottom": "30%",
+          "margin-bottom": "-36px"
+        }, function () {
           $(this).addClass("clicked");
         });
     });
   });
   //clique do "x" no overlay da voz
-  $(".voice-close").click(function(){
+  $(".voice-close").click(function () {
     $(".voice-search").removeClass("clicked").animate({
-        "bottom":"2em",
-        "margin-bottom":"0"
-      }, function(){
-        $(".voice-overlay").fadeOut();
-      });
+      "bottom": "2em",
+      "margin-bottom": "0"
+    }, function () {
+      $(".voice-overlay").fadeOut();
+    });
   });
   
-  $(".map-close").click(function(){
+  $(".map-close").click(function () {
     $(".map-overlay").fadeOut();
   });
   //variaveis
-  var pesq = '', 
-      json = '',
-      isMobile = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function () {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function () {
-    return navigator.userAgent.match(/iPhone|iPod/i);
-  },
-  Opera: function () {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function () {
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function () {
-    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-  }
-},
-      mob = isMobile.any();
-  
+  var pesq = '',
+    json = '',
+    isMobile = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPod/i);
+      },
+      Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+    },
+    mob = isMobile.any();
+
   //clique do botão de pesquisa normal
-  $(".search-button").click(function(){
+  $(".search-button").click(function () {
     pesq = $("#pesq").val();
-    if (pesq != "") {
-      
-      var contResult = 0
-        ,linhas = $.getJSON("linhas.json", function(data){
-        $.each(data.linhas, function(i, item){
-          if(this.nome.indexOf(pesq) > -1 || this.id.indexOf(pesq) > -1 || this.itinerario.indexOf(pesq) > -1){
+    var contResult = 0,
+      linhas = $.getJSON("linhas.json", function (data) {
+        $.each(data.linhas, function (i, item) {
+          if (this.nome.indexOf(pesq) > -1 || this.id.indexOf(pesq) > -1 || this.itinerario.indexOf(pesq.toUpperCase()) > -1) {
             contResult++;
-            result.append("<div class=\"result\">"+
-                            "<header class=\"result-header\">"+
-                            "  <div class=\"linha\">"+ this.numero +"</div>"+
-                            "  <div class=\"nome\">"+ this.nome.replace('/','-') +"</div>"+
-                            "  <div class=\"sentido\">"+ this.sentido.replace('/','-') +"</div>"+
-                            "  <div class=\"horarios\"><i class=\"fa fa-clock-o\"></i><span>Horários</span>"+
-                            "  </div>"+
-                            "  <div class=\"trajetos\"><a href=\""+ this.gmaps +"\" target=\"_blank\"><i class=\"fa fa-map-marker\"></i> <span>Trajetória</span></a>"+
-                            "  </div>"+
-                            "</header>"+
-                          "</div>");
+            result.append("<div class=\"result\">" +
+              "<header class=\"result-header\">" +
+              "  <div class=\"linha\">" + this.numero + "</div>" +
+              "  <div class=\"nome\">" + this.nome.replace('/', '-') + "</div>" +
+              "  <div class=\"sentido\">" + this.sentido.replace('/', '-') + "</div>" +
+              "  <div class=\"horarios\" data-more=\""+contResult+"\"><i class=\"fa fa-clock-o\"></i><span>Horários</span>" +
+              "  </div>" +
+              "  <div class=\"trajetos\"><a href=\"" + this.gmaps + "\" target=\"_blank\"><i class=\"fa fa-map-marker\"></i> <span>Trajetória</span></a>" +
+              "  </div>" +
+              "</header>" +
+              "<div class=\"more-infos\" id=\""+contResult+"\">"+
+              " <section class=\"horarios\">"+
+                       loadHorarios(this.horarios) +
+              " </section>" +
+              "</div>"+
+              "</div>");
           };
         });
-        if(contResult == 0)
-          alert("Não encontramos nada com " + pesq);
-        else{          
-          $(".horarios").click(function(){
+        if (contResult == 0)
+          $(".message").addClass("error").removeClass("success").fadeIn();
+        else {
+          $(".message").fadeOut();
+          $(".horarios").click(function () {
             toggleMore($(this).data("more"));
           });
-          
+
           $("#pesq-query").html(pesq);
-          $(".results").fadeIn(function(){
+          $(".results").fadeIn(function () {
             $('html,body').animate({
               scrollTop: $(this).offset().top
-            }, 1000);  
+            }, 1000);
           });
         }
-      })
-      , result = $(".results .container");
-    }
-    else{
-      alert("Ô SEU CUZAO, DIGITA UMA COISA AE CARAIO!");
-    }
+      }),
+      result = $(".results .container");
+
   });
   
-  var element = $('.search-controls');
+  function loadHorarios(horarios){
+    var qtdHorarios = horarios.length,
+        htmlHorarios = '';
     
+    for(var i = 0; i < qtdHorarios; i++){
+      var listaHorario = horarios[i].horario.split('**'),
+          tamListaHorario = listaHorario.length,
+          htmlResult = "<div class=\"horario\">"+
+                         "<h2>"+listaHorario[0]+"</h2>"+
+                         "<table>"+
+                         "</table>"+
+                       "</div>";
+      
+      //o for vai começar do 1 pois o primeiro sempre vai ser o "título" da tabela, por exemplo, "de segunda a sexta-feira"
+      for(var y = 1; y < tamListaHorario; y++){
+          
+      }
+      
+      htmlHorarios += htmlResult;
+    }
+    
+    return htmlHorarios;
+  }
+  
+  var element = $('.search-controls');
+
   /*$(window).scroll(function () {
     if ($(this).scrollTop() > window.innerHeight - 1){
       element.addClass("fixed-controls");
@@ -114,43 +139,36 @@ $(function(){
       $(".show-controls, .back-to-top").fadeOut();
     }
   });*/
-  
-  $(".show-controls").click(function(){
+
+  $(".show-controls").click(function () {
     var x = $(".fixed-controls");
-    if($(x).is(":visible")){
+    if ($(x).is(":visible")) {
       $(this).html("<i class=\"fa fa-search\"></i>");
       $(x).fadeOut();
-    }
-    else{ 
-      $(this).html("<i class=\"fa fa-close\"></i>");  
+    } else {
+      $(this).html("<i class=\"fa fa-close\"></i>");
       $(x).fadeIn();
     }
   });
-  
-  $(".back-to-top").click(function(){
+
+  $(".back-to-top").click(function () {
     $('html,body').animate({
       scrollTop: $(".results").offset().top
-    }, 1000); 
+    }, 1000);
   });
-  
-  $(".result-header").click(function(){
-    if($(this).hasClass("visible-info"))
-      $(this).removeClass("visible-info");
-    else $(this).addClass("visible-info");
-  });
-  
-  function toggleMore(id){
-    var el = $("#"+ id);
-    if(el.is(":visible"))
+
+  function toggleMore(id) {
+    var el = $("#" + id);
+    if (el.is(":visible"))
       el.slideUp();
     else el.slideDown();
-    
+
     $('html,body').animate({
-          scrollTop: el.offset().top
-        }, 1000);
+      scrollTop: el.offset().top
+    }, 1000);
   }
   
-  function toggleMap(map){
-    
-  }
+  $(".close-message").click(function(){
+    $(".message").fadeOut();
+  });
 }(jQuery));
