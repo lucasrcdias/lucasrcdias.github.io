7deee508cb0af9fd814b7822749f6a0e56a24d8e
Lucas Ramos <lucasrcdias@gmail.com>
2015-03-29 10:40:21 -0300

    Inclusão de pesquisa por voz e organização de documentos

--- js/app.js
+++ js/app.js
@@ -1,36 +1,5 @@
 $(function () {
-  //clique no botão de pesquisa por voz
-  $(".search-toggle").click(function () {
-    $(".voice-overlay").fadeIn(function () {
-      if (!mob)
-        $(".voice-search").animate({
-          "bottom": "50%",
-          "margin-bottom": "-36px"
-        }, function () {
-          $(this).addClass("clicked");
-        });
-      else
-        $(".voice-search").animate({
-          "bottom": "30%",
-          "margin-bottom": "-36px"
-        }, function () {
-          $(this).addClass("clicked");
-        });
-    });
-  });
-  //clique do "x" no overlay da voz
-  $(".voice-close").click(function () {
-    $(".voice-search").removeClass("clicked").animate({
-      "bottom": "2em",
-      "margin-bottom": "0"
-    }, function () {
-      $(".voice-overlay").fadeOut();
-    });
-  });
-  
-  $(".map-close").click(function () {
-    $(".map-overlay").fadeOut();
-  });
+
   //variaveis
   var pesq = '',
     json = '',
@@ -57,29 +26,71 @@ $(function () {
     mob = isMobile.any();
 
   //clique do botão de pesquisa normal
-  $(".search-button").click(function () {
+
+  function loadHorarios(horarios) {
+    var qtdHorarios = horarios.length,
+      htmlHorarios = '';
+
+    for (var i = 0; i < qtdHorarios; i++) {
+      var listaHorario = horarios[i].horario.split('**'),
+        tamListaHorario = listaHorario.length,
+        htmlResult = "<div class=\"horario\">" +
+        "<h2>" + listaHorario[0] + "</h2>" +
+        "<table>" +
+        "  <thead>" +
+        "    <tr><td>" + listaHorario[1] + "</td><td>" + listaHorario[2] + "</td><td>" + listaHorario[3] + "</td><td>" + listaHorario[4] + "</td></tr>" +
+        "</thead>" +
+        "<tbody>";
+      //o for vai começar do 5, lembrando que por ser uma lista o primeiro índice é 0, então vamos começar do 6 item ou seja, 5º indice pois os 5 primeiros item (0, 4) são padrões.
+      //vou decrementar 4 pois quero que ele vá até o último item - 4 para que eu possa avançar manualmente dentro do for.
+      tamListaHorario -= 4;
+      for (var y = 5; y < tamListaHorario; y += 4) {
+        htmlResult += "<tr>";
+        if (parseInt(listaHorario[y].split(":")[0]) <= 6)
+          htmlResult += "<td>" + listaHorario[y] + "</td>";
+        else htmlResult += "<td></td>";
+        if (parseInt(listaHorario[y + 1].split(":")[0]) <= 12)
+          htmlResult += "<td>" + listaHorario[y + 1] + "</td>";
+        else htmlResult += "<td></td>";
+        if (parseInt(listaHorario[y + 2].split(":")[0]) <= 18)
+          htmlResult += "<td>" + listaHorario[y + 2] + "</td>";
+        else htmlResult += "<td></td>";
+        if (parseInt(listaHorario[y + 3].split(":")[0]) <= 24)
+          htmlResult += "<td>" + listaHorario[y + 3] + "</td>";
+        else htmlResult += "<td></td>";
+        htmlResult += "</tr>";
+      }
+      htmlResult += "   </tbody>" +
+        " </table>" +
+        "</div>";
+      htmlHorarios += htmlResult;
+    }
+
+    return htmlHorarios;
+  }
+
+  function search(s) {
     $(".result").remove();
-    pesq = $("#pesq").val();
     var contResult = 0,
       linhas = $.getJSON("linhas.json", function (data) {
         $.each(data.linhas, function (i, item) {
-          if (this.nome.indexOf(pesq) > -1 || this.id.indexOf(pesq) > -1 || this.itinerario.indexOf(pesq.toUpperCase()) > -1) {
+          if (this.nome.indexOf(s.toUpperCase()) > -1 || this.id.indexOf(s) > -1 || this.itinerario.indexOf(s.toUpperCase()) > -1) {
             contResult++;
             result.append("<div class=\"result\">" +
               "<header class=\"result-header\">" +
               "  <div class=\"linha\">" + this.numero + "</div>" +
               "  <div class=\"nome\">" + this.nome.replace('/', '-') + "</div>" +
               "  <div class=\"sentido\">" + this.sentido.replace('/', '-') + "</div>" +
-              "  <div class=\"horarios\" data-more=\""+contResult+"\"><i class=\"fa fa-clock-o\"></i><span>Horários</span>" +
+              "  <div class=\"horarios\" data-more=\"" + contResult + "\"><i class=\"fa fa-clock-o\"></i><span>Horários</span>" +
               "  </div>" +
               "  <div class=\"trajetos\"><a href=\"" + this.gmaps + "\" target=\"_blank\"><i class=\"fa fa-map-marker\"></i> <span>Trajetória</span></a>" +
               "  </div>" +
               "</header>" +
-              "<div class=\"more-infos\" id=\""+contResult+"\">"+
-              " <section class=\"horarios\">"+
-                       loadHorarios(this.horarios) +
+              "<div class=\"more-infos\" id=\"" + contResult + "\">" +
+              " <section class=\"horarios\">" +
+              loadHorarios(this.horarios) +
               " </section>" +
-              "</div>"+
+              "</div>" +
               "</div>");
           };
         });
@@ -91,7 +102,7 @@ $(function () {
             toggleMore($(this).data("more"));
           });
 
-          $("#pesq-query").html(pesq);
+          $("#pesq-query").html(s);
           $(".results").fadeIn(function () {
             $('html,body').animate({
               scrollTop: $(this).offset().top
@@ -101,81 +112,7 @@ $(function () {
         }
       }),
       result = $(".results .container");
-
-  });
-  
-  function loadHorarios(horarios){
-    var qtdHorarios = horarios.length,
-        htmlHorarios = '';
-    
-    for(var i = 0; i < qtdHorarios; i++){
-      var listaHorario = horarios[i].horario.split('**'),
-          tamListaHorario = listaHorario.length,
-          htmlResult = "<div class=\"horario\">"+
-                         "<h2>"+listaHorario[0]+"</h2>"+
-                         "<table>"+
-                         "  <thead>"+
-                         "    <tr><td>"+listaHorario[1]+"</td><td>"+listaHorario[2]+"</td><td>"+listaHorario[3]+"</td><td>"+listaHorario[4]+"</td></tr>"+
-                         "</thead>"+
-                         "<tbody>";
-      //o for vai começar do 5, lembrando que por ser uma lista o primeiro índice é 0, então vamos começar do 6 item ou seja, 5º indice pois os 5 primeiros item (0, 4) são padrões.
-      //vou decrementar 4 pois quero que ele vá até o último item - 4 para que eu possa avançar manualmente dentro do for.
-      tamListaHorario -= 4;
-      for(var y = 5; y < tamListaHorario; y += 4){
-        htmlResult += "<tr>";
-          if(parseInt(listaHorario[y].split(":")[0]) <= 6)
-            htmlResult += "<td>"+listaHorario[y]+"</td>";
-          else htmlResult += "<td></td>";
-          if(parseInt(listaHorario[y + 1].split(":")[0]) <= 12)
-            htmlResult += "<td>"+listaHorario[y + 1]+"</td>";
-          else htmlResult += "<td></td>";
-          if(parseInt(listaHorario[y + 2].split(":")[0]) <= 18)
-            htmlResult += "<td>"+listaHorario[y + 2]+"</td>";
-          else htmlResult += "<td></td>";
-          if(parseInt(listaHorario[y + 3].split(":")[0]) <= 24)
-            htmlResult += "<td>"+listaHorario[y + 3]+"</td>";
-          else htmlResult += "<td></td>";
-        htmlResult += "</tr>";
-      }
-      htmlResult += "   </tbody>"+
-                    " </table>"+
-                    "</div>";    
-      htmlHorarios += htmlResult;
-    }
-    
-    return htmlHorarios;
   }
-  
-  var element = $('.search-controls');
-
-  /*$(window).scroll(function () {
-    if ($(this).scrollTop() > window.innerHeight - 1){
-      element.addClass("fixed-controls");
-      $(".show-controls, .back-to-top").fadeIn();
-    }
-    else{
-      element.removeClass("fixed-controls");
-      $(".search-controls").fadeIn();
-      $(".show-controls, .back-to-top").fadeOut();
-    }
-  });*/
-
-  $(".show-controls").click(function () {
-    var x = $(".fixed-controls");
-    if ($(x).is(":visible")) {
-      $(this).html("<i class=\"fa fa-search\"></i>");
-      $(x).fadeOut();
-    } else {
-      $(this).html("<i class=\"fa fa-close\"></i>");
-      $(x).fadeIn();
-    }
-  });
-
-  $(".back-to-top").click(function () {
-    $('html,body').animate({
-      scrollTop: $(".results").offset().top
-    }, 1000);
-  });
 
   function toggleMore(id) {
     var el = $("#" + id);
@@ -187,8 +124,139 @@ $(function () {
       scrollTop: el.offset().top
     }, 1000);
   }
-  
-  $(".close-message").click(function(){
+
+  $(".close-message").click(function () {
     $(".message").fadeOut();
+  });
+
+  $(".search-button").click(function () {
+    search($(".search-input").val());
+  });
+
+  //todo o código para reconhecimento de voz
+
+  showInfo('info_start');
+  var final_transcript = '';
+  var recognizing = false;
+  var ignore_onend;
+  var start_timestamp;
+
+
+
+  function upgrade() {
+    start_button.style.visibility = 'hidden';
+    showInfo('info_upgrade');
+  }
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+
+  function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+  }
+  var first_char = /\S/;
+
+  function capitalize(s) {
+    return s.replace(first_char, function (m) {
+      return m.toUpperCase();
+    });
+  }
+
+  function startButton(event) {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.lang = "pt-BR";
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    showInfo('info_allow');
+    showButtons('none');
+    start_timestamp = event.timeStamp;
+  }
+
+  function showInfo(s) {
+    $(".overlay h1").fadeOut();
+    if (s)
+      $("#" + s).fadeIn();
+    else
+      $("#" + s).fadeOut();
+
+  }
+
+  if (!('webkitSpeechRecognition' in window)) {
+    upgrade();
+  } else {
+    var recognition = new webkitSpeechRecognition();
+    recognition.interimResults = true;
+    recognition.onstart = function () {
+      recognizing = true;
+      showInfo('info_speak_now');
+    };
+    recognition.onerror = function (event) {
+      if (event.error == 'no-speech') {
+        showInfo('info_no_speech');
+        ignore_onend = true;
+      }
+      if (event.error == 'audio-capture') {
+        showInfo('info_no_microphone');
+        ignore_onend = true;
+      }
+      if (event.error == 'not-allowed') {
+        if (event.timeStamp - start_timestamp < 100) {
+          showInfo('info_blocked');
+        } else {
+          showInfo('info_denied');
+        }
+        ignore_onend = true;
+      }
+    };
+    recognition.onend = function () {
+      recognizing = false;
+      if (ignore_onend) {
+        return;
+      }
+      if (!final_transcript) {
+        showInfo('info_start');
+        return;
+      }
+      $(".overlay").fadeOut(function () {
+        search($("#final_span").text());
+      });
+    };
+    recognition.onresult = function (event) {
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      final_transcript = capitalize(final_transcript);
+      final_span.innerHTML = linebreak(final_transcript);
+      interim_span.innerHTML = linebreak(interim_transcript);
+    };
+  }
+
+  $(".voice-button").click(function () {
+    $(".overlay").fadeIn();
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.lang = "pt-BR";
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    showInfo('info_allow');
+    start_timestamp = event.timeStamp;
+  });
+  $(".voice-cancel").click(function () {
+    $(".overlay").fadeOut();
   });
 }(jQuery));
